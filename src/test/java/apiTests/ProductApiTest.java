796ebe8e9f01ebd751c58caf86ba49d896ae4343
BNMK EH <csemoe@gmail.com>
2026-05-22 13:09:36 +0300

    Fix API GET test endpoint 8

--- src/test/java/apiTests/ProductApiTest.java
+++ src/test/java/apiTests/ProductApiTest.java
@@ -1,27 +1,36 @@
 package apiTests;
 
+import io.qameta.allure.*;
 import io.restassured.response.Response;
 import org.junit.Assert;
 import org.junit.Test;
 import utils.ApiClient;
-import io.qameta.allure.*;
 
 @Epic("API Tests")
 @Feature("Product API")
-public class ProductApiTest {
 
+public class ProductApiTest {
 
     @Test
     @Severity(SeverityLevel.NORMAL)
     @Description("Verify users list API")
+
     public void getUsersTest() {
 
-        Response response = ApiClient.get("/api/users?page=2");
+        Response response =
+                ApiClient.get("/api/users?page=2");
 
         System.out.println(response.asPrettyString());
 
-        Assert.assertEquals(200, response.statusCode());
+        Assert.assertEquals(
+                200,
+                response.statusCode()
+        );
 
-        Assert.assertTrue(response.jsonPath().getList("data").size() > 0);
+        Assert.assertTrue(
+                response.jsonPath()
+                        .getList("data")
+                        .size() > 0
+        );
     }
 }
